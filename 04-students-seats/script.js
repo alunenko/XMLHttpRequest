d9f7f109e11fb9f6cf169d36bb9065ce607e2ac9
Antoni Lunenko <antoni.lunenko@gmail.com>
2015-08-15 23:26:16 +0300

    student seats; in debug/err mode

---
+++ 04-students-seats/script.js
@@ -0,0 +1,101 @@
+window.onload = initAll;
+
+var xhr = false,
+	dataArray = new Array(),
+	url = "students.xml";
+
+function initAll() {
+	if (window.XMLHttpRequest) {
+		xhr = new XMLHttpRequest();
+	} else {
+		/* for IE 6 */
+		if (window.ActiveXObject) {
+			try {
+				xhr = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			catch (e) {  }
+		}
+	}
+
+	if (xhr) {
+		xhr.onreadystatechange = setDataArray;
+		xhr.open("GET", url, true);
+		xhr.send(null);
+	} else {
+		alert("Sorry, but I couldn't create an XMLHttpRequest");
+	}
+
+	var allItems = document.getElementsByTagName("li");
+
+	for (var i = 0; i < allItems.length; i++) {
+		allItems[i].onclick = featureOneItem;
+	};
+}
+
+function setDataArray() {
+	if (xhr.readyState == 4) {
+		if (xhr.status == 200) {
+			if (xhr.responseXML) {
+				var allData = xhr.responseXML.getElementsByTagName("student");debugger;
+
+				for (var i = 0; i < allData.length; i++) {
+					var tempObj = new Object;
+
+					tempObj.firstName = getVal(allData[i], "firstName");
+					tempObj.lastName = getVal(allData[i], "lastName");
+					tempObj.seat = getVal(allData[i], "seat");
+					tempObj.lunchPeriod = getVal(allData[i], "lunchPeriod");
+					tempObj.readingGroup = getVal(allData[i], "readingGroup");
+
+					dataArray[i] = tempObj;
+				};
+			}
+		} else {
+			alert("There was a problem with the request: readyState(" + 
+				xhr.readyState + "), status(" + 
+				xhr.status + ")");
+		}
+	}
+
+	function getVal(theData, theTag) {
+		return theData.getElementsByTagName(theTag)[0].firstChild.nodeValue;
+	}
+}
+
+function featureOneItem(e) {
+	var allItems = document.getElementsByTagName("li");
+
+	for (var i = 0; i < allItems.length; i++) {
+		allItems[i].className = '';
+	};
+
+	var theItem = (e) ? e.target : window.event.srcElement;
+	theItem.className = "pickedDiv";
+
+	var student = null;
+
+	for (var i = 0; i < dataArray.length; i++) {
+		if(theDiv.id == dataArray[i].seat) {
+			student = dataArray[i];
+		}
+	}
+
+	if(theStudent) {
+		var studentInfo = document.getElementById("detail"),
+			theMsg = "<span id='closeBox'>X</span><h3>";
+
+		theMsg += theStudent.firstName + " " + theStudent.lastName;
+		theMsg += "</h3>Seat: " + theStudent.seat;
+		theMsg += "<br /> Lunch Period: " + theStudent.lunchPeriod;
+		theMsg += "<br /> Reading Group: " + theStudent.readingGroup;
+		studentInfo.innerHTML = theMsg;
+
+		studentInfo.style.top = (theDiv.offsetTop-5) + "px";
+		studentInfo.style.left = (theDiv.offsetLeft-35) + "px";
+		studentInfo.style.visibility = "visible";
+
+		document.getElementById("closeBox").onclick = function() {
+			document.getElementById("detail").style.visibility = "hidden";
+		};
+	}
+}
